744be6bac2b50e44c670f9a632b5e0dd5ee85265
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2025-09-09 21:13:16 -0400

    Add brain slice images and coordinate mappings

---
+++ ios-app/NeuroAtlas/NeuroAtlasApp.swift
@@ -0,0 +1,11 @@
+// NeuroAtlasApp.swift - Main App Entry Point
+import SwiftUI
+
+@main
+struct NeuroAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
